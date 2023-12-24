83ef1c443fb338d2e68c468adec9f3ee4be39e2f
omarmrad01 <mardomar36@gmail.com>
2023-12-24 19:25:57 +0100

    done

--- function.js
+++ function.js
@@ -1,1 +1,49 @@
-
+// Q1 we creat variable
+var carName='volvo'
+//Q4 we write javascript program
+function largest(num1,num2){
+largest=Math.max(num1,num2)
+return largest 
+}
+//Q4 write a javascript program
+function largest(num1,num2){
+    return Math.max(num1,num2)
+}
+//Q5 we create a function called add
+function add(num1,num2){
+    var num1=2
+    var num2=3
+    return add=num1+num2
+}
+//Q6 we create a function called subtract
+function subtract(num1,num2){
+    var num1=2
+    var num2=3
+    return add=num1-num2
+}
+//Q7 we write two function add and subtract
+//step1: create a variable called operator
+var operator='+'
+//step2: we create two variable 
+var num1=2
+var num2=1
+//step3: create if/else
+if (operator==='+'){
+    console.log(add(num1,num2))}
+else {
+    console.log(subtract(num1,num2))
+}
+//step4: write a function for add and subtract
+function add(num1,num2){
+    return num1+num2
+}
+function subtract(num1,num2){
+    return num1-num2
+}
+//Q8 create two function divide and multiply
+function divide(num1,num2){
+    return num1*num2
+}
+function multiply(num1,num2){
+    return num1/num2
+}
